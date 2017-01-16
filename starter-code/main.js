3ec7e625131aa3547ac1efaa9ea53e0303579a9d
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-01-16 16:48:37 -0500

    created card check logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,8 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardOne===cardThree){
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
