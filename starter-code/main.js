16d13a72bb020da7867556b367e8ac35e23d0f90
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-01-16 21:11:22 -0500

    added card arrays and functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,37 @@
-var createCards = function(cardOne,cardTwo,cardThree,cardFour){
+var createBoard = function(cards){
 	var gameboard = document.getElementById('game-board');
-	for (var i=1; i<=4; i++){
+	for (var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
 		newCard.className = 'card';
+		cardElement.setAttribute('data-card',cards[i]);
+		cardElement.addEventListener('click',isTwoCards);
+		if (cards[i]==='king') {
+			cardElement.innerHTML = '<img src="king.png" alt="King" />';
+		}
+		else {
+			cardElement.innerHTML = '<img src="queen.png" alt="Queen" />';
+		}
 		gameboard.appendChild(newCard);
 	}
 }
-createCards('queen','queen','king','king')
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+	}
+	cardsInPlay = [];
+}
+
+var isMatch = function(cardsInPlay){
+	if (cardsInPlay[0]===cardsInPlay[1]){
+		alert("It's a match!");
+	}
+	else {
+		alert("Try again.");
+	}
+}
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+createBoard(cards);
